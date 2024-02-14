c83f567dac005a58e01670ca050942169a28a433
rollo <rollovitch.roll@yandex.ru>
2024-02-14 19:08:14 +0300

    Init

---
+++ src/main/java/com/labs/ziptodb/ZipToDbApplication.java
@@ -0,0 +1,13 @@
+package com.labs.ziptodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipToDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipToDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labs/ziptodb/ZipToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.ziptodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipToDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
